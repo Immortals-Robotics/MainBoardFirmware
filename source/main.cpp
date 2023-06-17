accb3774ac876770547ecd0810f39a86ddc120c4
Ali Salehi <nysalehi@gmail.com>
2023-06-17 11:50:23 +0200

    Add micro spi test code.

--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,7 @@
 #include <cstdio>
 
+#include <pigpiod_if2.h>
+
 #include "spi_hal.h"
 
 extern "C" {
@@ -7,7 +9,7 @@ extern "C" {
 #include "tmc/ic/TMC6200/TMC6200.h"
 }
 
-int main(int argc, char* argv[])
+void motors_test()
 {
     init_spi();
 
@@ -27,6 +29,47 @@ int main(int argc, char* argv[])
     enable_driver(true);
 
     shutdown_spi();
+}
+
+void micro_test()
+{
+    const int pi = pigpio_start(0, 0); /* Connect to local Pi. */
+
+    if (pi < 0)
+    {
+        printf("Can't connect to pigpio daemon\n");
+        return;
+    }
+
+    char rx_buf[0x100] = {};
+
+    char msg[0x100] = {};
+    for (int i = 0; i < 0x100; ++i)
+    {
+        // bit-inverted from i. The values should be: {0xff, 0xfe, 0xfd...}
+        msg[i] = ~i;
+    }
+
+    int h = spi_open(pi, 1, 8 * 1000 * 1000, PI_SPI_FLAGS_AUX_SPI(1));
+
+    if (h < 0)
+        return;
+
+    while (true)
+    {
+        spi_xfer(pi, h, msg, rx_buf, 0x100);
+        printf("rcv: %s", rx_buf);
+    }
+
+    spi_close(pi, h);
+
+    pigpio_stop(pi); /* Disconnect from local Pi. */
+}
+
+int main(int argc, char* argv[])
+{
+    //motors_test();
+    micro_test();
 
     return 0;
 }
