391e8d4658c6b5aa88375ba9526abd2a027a3780
Ali Salehi <nysalehi@gmail.com>
2023-06-18 15:36:29 +0200

    Move motor functionality to its own class.

---
+++ source/motor.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <cstdint>
+
+class Motor
+{
+public:
+    enum class MotionMode : uint8_t
+    {
+        Unknown,
+        Velocity,
+        Position,
+        Stopped,
+    };
+
+    Motor(uint8_t t_id);
+    ~Motor() = default;
+
+    bool init();
+
+    void                     setMotionMode(MotionMode t_mode);
+    [[nodiscard]] MotionMode getMotionMode() const;
+
+    void              setTargetVelocity(int t_velocity);
+    [[nodiscard]] int getTargetVelocity() const;
+
+    [[nodiscard]] int getActualVelocity() const;
+
+private:
+    bool initController();
+    bool initDriver();
+
+    int m_id;
+};
